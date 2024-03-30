b8f7acc8073275c1646ee6881f158b01f92b11ef
Leslie Abajoli <leslie.abajoli1989@gmail.com>
2024-03-30 20:45:44 +0100

    added style navbar

--- Frontend/src/components/HomeSection.jsx
+++ Frontend/src/components/HomeSection.jsx
@@ -5,6 +5,7 @@ export default function HomeSection() {
         <div id="section-home">
           <p className="title">Welcome</p>
           <p className="title"> Discover all my projects</p>
+          <img src="./assets/blue-wallpaper.jpg" alt="" />
         </div>
       </div>
     </>
