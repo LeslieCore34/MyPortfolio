76a6b994d482c2c9c3e8415dfcc4bda9d5721637
Leslie Abajoli <leslie.abajoli1989@gmail.com>
2024-04-10 17:42:31 +0200

    added link and style component street-art

--- Frontend/src/components/StreetArt.jsx
+++ Frontend/src/components/StreetArt.jsx
@@ -7,18 +7,19 @@ export default function StreetArt() {
       <p className="project-description">
         Street Art Hunters is a project I developed with two classmates during
         my studies. The application features a user interface built with React
-        and styled using SCSS. To facilitate communication with the backend, we
-        implemented a REST API using the Express framework on Node.js. The
-        project utilizes a MySQL database for data storage.
+        and styled using SCSS. We implemented a REST API using the Express
+        framework on Node.js. The project utilizes a MySQL database for data
+        storage.
       </p>
 
-      <p>
+      <p className="project-use-description">
         Use your camera to capture the city's street art, and rack up points for
         your discoveries. Track your progress and see how you stack up against
         other players on the global leaderboard!
       </p>
 
       <Link
+        className="link-streetart"
         to="https://street-art-hunter.laloupe-1.wilders.dev/map"
         target="_blank"
       >
