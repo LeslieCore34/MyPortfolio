c76bd9b0b4df1bded423192d8ba3e245c762b5f9
Leslie Abajoli <leslie.abajoli1989@gmail.com>
2024-04-15 21:53:25 +0200

    added responsive

--- Frontend/src/components/Eshop.jsx
+++ Frontend/src/components/Eshop.jsx
@@ -7,34 +7,37 @@ export default function Eshop() {
       <div className="image-container">
         <div className="project-image"></div>
       </div>
-      <h2 className="project-description">Project description :</h2>
 
-      <ul className="functionnalities">
-        <li>Multiple products pages</li>
-        <li>Stripe for (fake) payment</li>
-        <li>Secured authentication with Tokens</li>
-        <li>Password stored using bcrypt</li>
-        <li>Download of images in database</li>
-        <li>Relational Database with MySQL</li>
-        <li>MVC architecture</li>
-      </ul>
-      <div className="project-use-description">
-        <p>
-          <span>Eshop</span> is a project I am currently developping. This web
-          application features a user interface built with React and styled
-          using SCSS. I implemented a REST API using the Express framework and
-          Node.js.
-          <p className="how-to-use">
-            You can browse through the articles and (almost) purchase them !
+      <div className="list-container">
+        <h2 className="project-description">Project description :</h2>
+
+        <ul className="functionnalities">
+          <li>Multiple products pages</li>
+          <li>Stripe for (fake) payment</li>
+          <li>Secured authentication with Tokens</li>
+          <li>Password stored using bcrypt</li>
+          <li>Download of images in database</li>
+          <li>Relational Database with MySQL</li>
+          <li>MVC architecture</li>
+        </ul>
+        <div className="project-use-description">
+          <p>
+            <span>Eshop</span> is a project I am currently developping. This web
+            application features a user interface built with React and styled
+            using SCSS. I implemented a REST API using the Express framework and
+            Node.js.
+            <p className="how-to-use">
+              You can browse through the articles and (almost) purchase them !
+            </p>
           </p>
-        </p>
-        <Link
-          className="link-eshop"
-          to="https://street-art-hunter.laloupe-1.wilders.dev/map"
-          target="_blank"
-        >
-          Enjoy your shopping!
-        </Link>
+          <Link
+            className="link-eshop"
+            to="https://github.com/LeslieCore34/Eshop"
+            target="_blank"
+          >
+            Checkout my code!
+          </Link>
+        </div>
       </div>
     </div>
   );
--- Frontend/src/components/StreetArt.jsx
+++ Frontend/src/components/StreetArt.jsx
@@ -7,32 +7,34 @@ export default function StreetArt() {
       <div className="image-container">
         <div className="project-image"></div>
       </div>
-      <h2 className="project-description">Project description :</h2>
-      <ul className="functionnalities">
-        <li>Google Maps Integration</li>
-        <li>Real-time Geolocation</li>
-        <li>Photo functionality to take pictures</li>
-        <li>Secured authentication with Tokens</li>
-        <li>Password stored using bcrypt</li>
-        <li>Relational Database with MySQL</li>
-        <li>MVC architecture</li>
-      </ul>
-      <div className="project-use-description">
-        <p>
-          <span>Street-Art Hunters</span> is a project I developed with two
-          classmates during my studies. <br />
-          <p className="how-to-use">
-            Use your camera to capture city street art, and accumulate points
-            for each discovery!
+      <div className="list-container">
+        <h2 className="project-description">Project description :</h2>
+        <ul className="functionnalities">
+          <li>Google Maps Integration</li>
+          <li>Real-time Geolocation</li>
+          <li>Photo functionality to take pictures</li>
+          <li>Secured authentication with Tokens</li>
+          <li>Password stored using bcrypt</li>
+          <li>Relational Database with MySQL</li>
+          <li>MVC architecture</li>
+        </ul>
+        <div className="project-use-description">
+          <p>
+            <span>Street-Art Hunters</span> is a project I developed with two
+            classmates during my studies. <br />
+            <p className="how-to-use">
+              Use your camera to capture city street art, and accumulate points
+              for each discovery!
+            </p>
           </p>
-        </p>
-        <Link
-          className="link-streetart"
-          to="https://street-art-hunter.laloupe-1.wilders.dev/map"
-          target="_blank"
-        >
-          Start to play!
-        </Link>
+          <Link
+            className="link-streetart"
+            to="https://street-art-hunter.laloupe-1.wilders.dev/map"
+            target="_blank"
+          >
+            Start to play!
+          </Link>
+        </div>
       </div>
     </div>
   );
