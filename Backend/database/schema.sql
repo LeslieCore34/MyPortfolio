fb6b78ac14e13885ec269faeedb271672606d6d4
Leslie Abajoli <leslie.abajoli1989@gmail.com>
2024-04-15 13:06:49 +0200

    deleted backend

--- Backend/database/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `projects` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `image` BLOB,
-  `tech-description` VARCHAR(100),
-  `project-description` VARCHAR(100),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
